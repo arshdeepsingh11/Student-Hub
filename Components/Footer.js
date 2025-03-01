2885d27f9c1a2636d6be88a6f9cc01e16d10e4c3
Gyanit7 <gyanitv@gmail.com>
2025-02-28 23:25:19 -0700

    Created Next.js project

--- Components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-4">
-      <p>New Generation High School &copy; {new Date().getFullYear()}</p>
-      <p>123 School Street, City, Country</p>
-    </footer>
-  );
-}
